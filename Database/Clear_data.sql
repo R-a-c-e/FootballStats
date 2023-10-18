33210a688b7a07143e1a40025d82212d0567e255
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2023-10-18 14:33:33 -0700

    Added file to clear database

---
+++ Database/Clear_data.sql
@@ -0,0 +1,56 @@
+DELETE FROM Offensive_Plays;
+
+DELETE FROM Defensive_Plays;
+
+DELETE FROM Defense_Turnovers;
+
+DELETE FROM Downs;
+
+DELETE FROM Special_Teams;
+
+DELETE FROM Field_Goals;
+
+DELETE FROM Kickoff;
+
+DELETE FROM Kickoff_Return;
+
+DELETE FROM Punt;
+
+DELETE FROM Punt_Return;
+
+DELETE FROM Team;
+
+DELETE FROM Play_Type;
+
+DELETE FROM Side;
+
+DELETE FROM FG_Ranges;
+
+
+DROP TABLE Offensive_Plays;
+
+DROP TABLE Defensive_Plays;
+
+DROP TABLE Defense_Turnovers;
+
+DROP TABLE Downs;
+
+DROP TABLE Special_Teams;
+
+DROP TABLE Field_Goals;
+
+DROP TABLE Kickoff;
+
+DROP TABLE Kickoff_Return;
+
+DROP TABLE Punt;
+
+DROP TABLE Punt_Return;
+
+DROP TABLE Team;
+
+DROP TABLE Play_Type;
+
+DROP TABLE Side;
+
+DROP TABLE FG_Ranges;
